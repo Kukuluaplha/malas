9f7e4364397ed1096b811d822ca20ac3e2e176a8
Kukuluaplha <121417937+Kukuluaplha@users.noreply.github.com>
2023-01-04 15:57:37 +0000

    Create gas.sh

---
+++ gas.sh
@@ -0,0 +1,9 @@
+sudo apt update 
+sudo apt upgrade -y
+sudo apt install neofetch
+sudo apt install tigervnc-standalone-server
+sudo apt install xrdp
+sudo apt install nano lxde ssh gedit 
+sudo apt install wget curl git dbus-x11
+sudo apt install firefox-esr
+sudo apt install firefox
